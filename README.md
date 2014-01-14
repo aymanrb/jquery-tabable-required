66e9aee084a32177cb2548b737cfb8cfb81584f6
Ayman Bedair <aymanrb@gmail.com>
2014-01-14 18:19:34 +0200

    Initating the project

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-jquery-tabable-required
-=======================
+Tabable Required Fields
+================================
+A very simple jQuery plugin that prevents web forms from being submitted on 'Enter' keypress and replaces this default behavior with a more convenient one. It focuses on the next required field in the form. If no fields are required the plugin will take you through each input one at a time (just like the 'Tab' keypress behavior in this case) 
 
-A jQuery plugin that prevents web forms from being submitted on 'Enter' keypress and replaces the default behavior with a more convenient one
+# Currently Working On:
+
+	1. Integrating it with the [Flying Focus Effect](https://github.com/NV/flying-focus/)
+	2. Doing the reverse iteration on required elements on CTRL button press.
---
+++ js/jquery.required-focus.js
@@ -0,0 +1,34 @@
+(function($) {
+	'use strict';
+	
+	//On Page Load prevent the default Enter keypress action and get to the focus function
+	$(document).ready(function(){
+		$(this).on("keypress keydown", function(e) {
+		  var code = e.keyCode || e.which; 
+		  if (code == 13) {
+		    e.preventDefault();
+		    $.focusNextRequired();
+		  }
+		});
+	});
+
+	//Select the next required field in the current page
+	$.focusNextRequired = function(){
+		var focusableElements = $(':required');
+		
+		//Incase none of the document elements are required 
+		if(focusableElements.length == 0){
+			focusableElements = $(':input');
+		}
+		var current = $(':focus');
+		var nextIndex = 0;
+		console.log(current.length);
+		if(current.length === 1){
+			var currentIndex = focusableElements.index(current);
+			if(currentIndex + 1 < focusableElements.length){
+				nextIndex = currentIndex + 1;
+			}
+		}
+		requiredElements.eq(nextIndex).focus();
+	}
+})(jQuery);
