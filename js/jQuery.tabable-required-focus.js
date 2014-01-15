637f8a9fffaf3aa2b3b6f2ffd49b3d28a5479d8d
Ayman Bedair <aymanrb@gmail.com>
2014-01-15 15:13:52 +0200

    Updated the demo with the new Plugin code

--- js/jQuery.tabable-required-focus.js
+++ js/jQuery.tabable-required-focus.js
@@ -3,11 +3,14 @@
 	
 	//On Page Load prevent the default Enter keypress action and get to the focus function
 	$(document).ready(function(){
-		$(this).on("keypress keydown", function(e) {
+		$('form').on("keypress keydown", function(e) {
 		  var code = e.keyCode || e.which; 
-		  if (code == 13) {
+		  if (code == 13) { //If 'Enter'
 		    e.preventDefault();
 		    $.focusNextRequired();
+		 }else if (code == 17) { //If 'CTRL'
+		    e.preventDefault();
+		    $.focusPrevRequired();
 		  }
 		});
 	});
@@ -27,6 +30,26 @@
 			var currentIndex = focusableElements.index(current);
 			if(currentIndex + 1 < focusableElements.length){
 				nextIndex = currentIndex + 1;
+			}
+		}
+		focusableElements.eq(nextIndex).focus();
+	}
+	
+	//Select the next required field in the current page
+	$.focusPrevRequired = function(){
+		var focusableElements = $(':required');
+		
+		//Incase none of the document elements are required 
+		if(focusableElements.length == 0){
+			focusableElements = $(':input');
+		}
+		var current = $(':focus');
+		var nextIndex = 0;
+		console.log(current.length);
+		if(current.length === 1){
+			var currentIndex = focusableElements.index(current);
+			if(currentIndex - 1 < focusableElements.length){
+				nextIndex = currentIndex - 1;
 			}
 		}
 		focusableElements.eq(nextIndex).focus();
